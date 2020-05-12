8da55bb78632f3d339383d84d00cd84016d8a5b2
BPurkaple <brpurkaple@gmail.com>
2020-05-12 17:33:20 -0500

    word search

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.listen(process.env.PORT || 80, function () {
   app.post("/api/incomingMsg", function(req, res) {
     var newMsg = req.body;
     console.log('request received:', req, newMsg);
-    if (newMsg.text.indexOf('Lucas') !== -1 && newMsg.sender_type !== 'bot') {
+    if (newMsg.group_id == "59549084") {
+      if (newMsg.text.indexOf('Lucas') !== -1 && newMsg.sender_type !== 'bot') {
         console.log('found Lucas');
         var formData = {  "bot_id"  : "b880562f84d27664eac33a6adc",
         "text"    : "Lucas is the Pope \n Wooooooooooooo!"}
@@ -27,6 +28,19 @@ app.listen(process.env.PORT || 80, function () {
 });
         // now construct an https post and execute it against the groupme api with our bot's token, etc.
     }
+  }
+  if (newMsg.group_id == "56143399") {
+    if(newMsg.text.toLowercase.indexOf('flotsam') !== -1 && newMsg.sender_type !== "bot"){
+      var formData = {"bot_id" : "eb79a1ada561478cdfcda0335d",
+       "text"    : "The winner is: " + name + "!" + '\nAnd the word was "flotsam",  which means: floating wreckage of a ship or its cargo'}
+        request.post({url:'https://api.groupme.com/v3/bots/post', formData: formData}, function optionalCallback(err, httpResponse, body) {
+  if (err) {
+    return console.error('upload failed:', err);
+  }
+  console.log('Upload successful!  Server responded with:', body);
+});
+    }
+  }
     res.status(201).json(newMsg);
 });
 //     app.get("/api/incomingMsg", function(req, res) {
